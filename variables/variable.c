3e0d88d822cb766fd392e11f6ae1c7c3817d6666
Nate-Castro <nate.castro@ucas-edu.net>
2024-09-23 19:11:10 +0000

    wv

--- variables/variable.c
+++ variables/variable.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
 int main(void){
-    char name = "Nate";
-    char num = "3";
-    char num2 = "294";
-    char breakfast = "Toast";
-    char color = "Purple";
-    char school = "UCAS";
-    char year = "2024";
-    char age = "14";
-    char subject = "Math";
-    printf(name, num, num2, breakfast, color, school, year, age, subject);
-    
+    char name[] = "Nate";
+    char num[] = "3";
+    char num2[] = "294";
+    char breakfast[] = "Toast";
+    char color[] = "Purple";
+    char school[] = "UCAS";
+    char year[] = "2024";
+    char eyes[] = "Brown";
+    char age[] = "14";
+    char subject[] = "Math";
+    printf ("%s\n", name);
+    printf ("%s\n", num);
+    printf ("%s\n", num2);
+    printf ("%s\n", breakfast);
+    printf("%s\n",color);
+    printf("%s\n", school);
+    printf("%s\n", year);
+    printf("%s\n", eyes);
+    printf("%s\n", age);
+    printf("%s\n", subject);
     return 0;
+
 }
