67f85bdec91a1c70dbc0591229a23d945278ed9d
Nate-Castro <nate.castro@ucas-edu.net>
2024-09-23 19:36:58 +0000

    ggg

--- Program stuff/first.c
+++ Program stuff/first.c
@@ -1,2 +1,9 @@
+#include <stdio.h>
 
-
+int main(void){
+    char favoriteColor[30]; 
+    printf("What is your favorite color?: \n");
+    fgets (favoriteColor,sizeof (favoriteColor),stdin); 
+    printf ("Nice %s\n", favoriteColor); 
+    return 0;
+}
