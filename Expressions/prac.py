45d67aac58984f281ecc84ed778cf8501cb0d22b
Nate-Castro <nate.castro@ucas-edu.net>
2024-09-24 19:16:05 +0000

    gfcvb

--- Expressions/prac.py
+++ Expressions/prac.py
@@ -8,6 +8,13 @@ f = 18
 g = 3
 h = 2
 i = 5
+j = 12
 print(b - a / c * d + e)
 print(f / g - b + h * i) 
-print()
+print(e * d / j + 72 / c - 9)
+print((17 - e / h) + d * g)
+print( -2 * (1 * d - h / h) + (e + h - g))
+print(-1 *  ((g - d * b) / i) - h * a / e)  
+print((g * i ** h / 15) - (i - h ** h))
+print((1 ** d * h ** h + g ** g) - h ** i / d)
+print((22 / h - h * i) ** h + (d - e  / e) ** h)
