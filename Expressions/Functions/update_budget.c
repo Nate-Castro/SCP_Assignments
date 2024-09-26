a866f19fa16ade69e3787d6efde4a3d2def127c3
Nate-Castro <nate.castro@ucas-edu.net>
2024-09-26 18:44:24 +0000

    hwed

--- Expressions/Functions/update_budget.c
+++ Expressions/Functions/update_budget.c
@@ -16,27 +16,30 @@ void percent(char type[], int amount){
 
 
 int main(void){
-printf("This is a budget calculator. \n");
-income = input("income", income);
-rent = input("rent", rent);
-untilities = input("utilities", untilities);
-grocceries = input("groceries", grocceries);
-transportation = input("transportation", transportation);
-savings = income * .2;
-expenses = rent + untilities + grocceries + transportation + savings;
-spend = income - expenses - savings;
-printf("You make $%.2f\n", income);
-printf("You make $%.2f\n", expenses);
-printf("You make $%.2f\n", savings);
-printf("Your spending money is $%.2f\n", spend);
+  printf("This is a budget calculator. \n");
+  
+  income = input("income", income);
+  rent = input("rent", rent);
+  untilities = input("utilities", untilities);
+  grocceries = input("groceries", grocceries);
+  transportation = input("transportation", transportation);
 
-percent("rent", rent);
-percent("untilities", untilities);
-percent("groceries", grocceries);
-percent("transportation.", transportation);
-percent("expenses",expenses);
-percent("savings", savings);
-percent("spending", spend);
+  savings = income * .2;
+  expenses = rent + untilities + grocceries + transportation + savings;
+  spend = income - expenses - savings;
+
+  printf("You make $%.2f\n", income);
+  printf("Your expenses $%.2f\n", expenses);
+  printf("You save $%.2f\n", savings);
+  printf("Your spending money is $%.2f\n", spend);
+
+  percent("rent", rent);
+  percent("untilities", untilities);
+  percent("groceries", grocceries);
+  percent("transportation.", transportation);
+  percent("expenses",expenses);
+  percent("savings", savings);
+  percent("spending", spend);
 
   return 0;
 }
