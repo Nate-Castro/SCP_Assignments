38f2e95ca1efdd26e25b18b02225bdf1061d4b62
Nate-Castro <nate.castro@ucas-edu.net>
2024-09-30 18:42:23 +0000

    trg

--- Expressions/finCalc.py
+++ Expressions/finCalc.py
@@ -1,2 +1,13 @@
+#Nathaniel Castro Financial calculuhtore
+
+print("Hello, welcome to your financial calculator!")
+income = input("What is your monthly income?: ")
+rent = input("What is your monthly rent?: ")
+utilities = input("What is your mobthly utilities?: ")
+groceries = input("What is your monthly groceries?: ")
+transportation = input("What is your monthly transportation?: ")
+inc = income * 1
+ren = rent * 
 
 
+print("Your monthly income is$", income)
