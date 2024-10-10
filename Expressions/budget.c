a89c3c90819391c8c3455d270e91ac0da91e66f4
Nate-Castro <nate.castro@ucas-edu.net>
2024-10-10 18:42:11 +0000

    e

--- Expressions/budget.c
+++ Expressions/budget.c
@@ -6,7 +6,7 @@ int main(void){
     printf("Hello user, welcom to your budget calculator.\n");
     printf("How much do you make each month?: ");
     scanf("%f", &income);
-    printf("How much is your rent?: ");     //Go to 8:30 on code with me video
+    printf("How much is your rent?: ");     //Go to 9:25 on code with me video
     scanf("%f", &rent);
     printf("How much is your utilities?: ");
     scanf("%f", &utilities);
@@ -14,13 +14,12 @@ int main(void){
     scanf("%f", &groceries);
     printf("How much is your transportation?: ");
     scanf("%f", &transportation);
-
     expenses = rent + utilities + groceries + transportation;
     savings = income * .2;
-
+    total = income - savings - expenses;
     printf("Your monthly income is: $%.2f\n", income);
     printf("Your monthly expenses are: $%.2f\n", expenses);
     printf("Your monthly savings is: $%.2f\n", savings);
-
+    printf("You have $%.2f\n", total, "left that you can spend");
     return 0;
 };
