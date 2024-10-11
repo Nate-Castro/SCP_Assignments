7238b349e6be9ad08eb5cec53912cc6949c97da9
Nate-Castro <nate.castro@ucas-edu.net>
2024-10-11 19:01:01 +0000

    sad

--- Expressions/budget.c
+++ Expressions/budget.c
@@ -20,13 +20,18 @@ int main(void){
     printf("Your monthly income is: $%.2f\n", income);
     printf("Your monthly expenses are: $%.2f\n", expenses);
     printf("Your monthly savings is: $%.2f\n", savings);
-    printf("You have $%.2f\n", total);
+    printf("You have $%.2f left to spend.\n", total);
     prent = rent/income * 100;
     putilities = utilities/income * 100;
     pgroceries = groceries/income * 100;
     ptransportation = transportation/income * 100;
     pexpenses = expenses/income * 100;
     psavings = savings/income * 100;
-
+    printf("Your expenses are %.1f%% of your income\n", pexpenses);
+    printf("Your rent is %.1f%% of your income\n", prent);
+    printf("Your utilities are %.1f%% of your income\n", putilities);
+    printf("Your groceries are %.1f%% of your income\n", pgroceries);
+    printf("Your transportation is %.1f%% of your income\n", ptransportation);
+    printf("Your savings are %.1f%% of your income\n", psavings);
     return 0;
 };
