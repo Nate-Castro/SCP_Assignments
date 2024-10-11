3459b97760f1907d791b8a33f1932a52cd3e5cdd
Nate-Castro <nate.castro@ucas-edu.net>
2024-10-11 19:30:29 +0000

    fsfds

--- string.py/notes/tes.c
+++ string.py/notes/tes.c
@@ -1,9 +1,9 @@
  #include <stdio.h>
-char sibs[2][20] = {"Eliana", "Juliette"};
+char sibs[2][20] = {" Eliana", "Juliette"};
 int i;
 int main(){
-    while(i<10){
-        printf("%s\n", sibs[i]);
+    while(i<2){
+        printf("Hello %s\n", sibs[i]);
         i++;
     }
     return 0;
