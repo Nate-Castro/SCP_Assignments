c8439dc2e133faab98e83d304d94f727a2fb5498
Nate-Castro <nate.castro@ucas-edu.net>
2024-10-15 18:43:06 +0000

    dsaca

--- Functions/Up_hello.py
+++ Functions/Up_hello.py
@@ -1,8 +1,14 @@
 name = input("What is your name?: ")
-def hello():
-    print("Hello", name)
+name1 = input("Give another name: ")
+name2 = input("Give another name: ")
+name3 = input("Give another name: ")
+name4 = input("Give another name: ")
+def hello(user):
+    print("Hello", user)
     return
 
-print(hello(), name)
-
-print(hello(), name)
+hello(name)
+hello(name1)
+hello(name2)
+hello(name3)
+hello(name4)
