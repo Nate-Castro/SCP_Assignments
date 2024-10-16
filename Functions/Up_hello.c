3a080d37b1f6a4b3e952f9eebb0e71bf5e9e2a08
Nate-Castro <nate.castro@ucas-edu.net>
2024-10-16 03:01:36 +0000

    hh

--- Functions/Up_hello.c
+++ Functions/Up_hello.c
@@ -1,17 +1,28 @@
 //Nathaniel Castro Update Hello World
 #include <stdio.h>
-char name[50];
-char name1[50];
-
+#include <string.h>
+char name[50], name1[50], name2[50], name3[50], name4[50];
+//char name1[50];
+//char name2[50];
+//char name3[50];
 int main(){
 
     printf("Give me a name: ");
-    scanf("%s", name, name1);
-    printf("Hello %s\n", name);
-
+    scanf("%s", name);
     printf("Give another name: ");
     scanf("%s", name1);
+    printf("Give another name: ");
+    scanf("%s", name2);
+    printf("Give another name: ");
+    scanf("%s", name3);
+    printf("Give another name: ");
+    scanf("%s", name4);
+
+    printf("Hello %s\n", name);
     printf("Hello %s\n", name1);
+    printf("Hello %s\n", name2);
+    printf("Hello %s\n", name3);
+    printf("Hello %s\n", name4);
 
     return 0;
 
