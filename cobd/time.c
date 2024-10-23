b001d9c4887d9847db7a3f29bbfbf70a7c1022ac
Nate-Castro <nate.castro@ucas-edu.net>
2024-10-23 18:42:32 +0000

    zXZ

---
+++ cobd/time.c
@@ -0,0 +1,15 @@
+//Nathaniel Castro Time of Day C
+
+#include <stdio.h>
+int time;
+
+int main(time){
+    printf("What time is it for you?: ");
+    scanf("%d\n", time);
+
+        if time <= 11:
+        print("Good morning");
+
+
+    return 0;
+}
